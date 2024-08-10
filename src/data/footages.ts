bdb315a50559b30075ab5328409bae1597b542b9
yogaprasss <yoga.prase.tyo@icloud.com>
2024-08-10 22:45:28 +0700

    adding lightbox for footage expanded

--- src/data/footages.ts
+++ src/data/footages.ts
@@ -1,15 +1,17 @@
-import Footage1Sm from '@/assets/footages/footage-1-sm.png';
-import Footage1Lg from '@/assets/footages/footage-1-lg.png';
-import Footage2Sm from '@/assets/footages/footage-2-sm.png';
-import Footage2Lg from '@/assets/footages/footage-2-lg.png';
-import Footage3Sm from '@/assets/footages/footage-3-sm.png';
-import Footage3Lg from '@/assets/footages/footage-3-lg.png';
-import Footage4Sm from '@/assets/footages/footage-4-sm.png';
-import Footage4Lg from '@/assets/footages/footage-4-lg.png';
-import Footage5Sm from '@/assets/footages/footage-5-sm.png';
-import Footage5Lg from '@/assets/footages/footage-5-lg.png';
-import Footage6Sm from '@/assets/footages/footage-6-sm.png';
-import Footage6Lg from '@/assets/footages/footage-6-lg.png';
+export const footageMobile = [
+  '/footages/footage-1-sm.png',
+  '/footages/footage-2-sm.png',
+  '/footages/footage-3-sm.png',
+  '/footages/footage-4-sm.png',
+  '/footages/footage-5-sm.png',
+  '/footages/footage-6-sm.png',
+];
 
-export const footageMobile = [Footage1Sm, Footage2Sm, Footage3Sm, Footage4Sm, Footage5Sm, Footage6Sm];
-export const footageDesktop = [Footage1Lg, Footage2Lg, Footage3Lg, Footage4Lg, Footage5Lg, Footage6Lg];
+export const footageDesktop = [
+  '/footages/footage-1-lg.png',
+  '/footages/footage-2-lg.png',
+  '/footages/footage-3-lg.png',
+  '/footages/footage-4-lg.png',
+  '/footages/footage-5-lg.png',
+  '/footages/footage-6-lg.png',
+];
--- src/section/Home/Footage.tsx
+++ src/section/Home/Footage.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
+import Lightbox from 'yet-another-react-lightbox';
 
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { Thumbnails, Zoom, Fullscreen } from 'yet-another-react-lightbox/plugins';
 import { footageMobile, footageDesktop } from '@/data/footages';
 
 import type { FC } from 'react';
-import type { StaticImageData } from 'next/image';
+import type { SlideImage } from 'yet-another-react-lightbox';
+
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
 
 interface FooterMobileProps {
-  images: StaticImageData[];
+  images: string[];
+  onClick: (index: number) => void;
 }
 
-const FootageMobile: FC<FooterMobileProps> = ({ images }) => {
+const FootageMobile: FC<FooterMobileProps> = ({ images, onClick }) => {
   const [indexImg, setIndexImg] = useState(0);
   
   useEffect(() => {
@@ -20,8 +26,10 @@ const FootageMobile: FC<FooterMobileProps> = ({ images }) => {
     return () => clearInterval(interval);
   }, []);
 
+  const onClickImg = useCallback(() => onClick(indexImg), [indexImg]);
+
   return (
-    <div className='w-full h-56 sm:h-96 relative block'>
+    <div className='w-full h-56 sm:h-96 relative block' onClick={onClickImg}>
       <Image
         src={images[indexImg]}
         alt='footage'
@@ -35,50 +43,87 @@ const FootageMobile: FC<FooterMobileProps> = ({ images }) => {
 };
 
 const Footage = () => {
+  const [isOpenLightbox, setIsOpenLightbox] = useState(false);
+  const [lightboxItems, setLightboxItems] = useState<SlideImage[]>([]);
+
+  const toggleLightbox = useCallback(() => {
+    setIsOpenLightbox((value) => !value);
+  }, []);
+
+  const initializeLighboxItems = useCallback((index: number, isMobile: boolean = false) => {
+    const list = (isMobile ? footageMobile : footageDesktop).map((src, index) => ({ index, src }));
+    const modifier = list.length - index;
+    const sortedList = list
+      .sort((a, b) => ((a.index + modifier) % list.length) - ((b.index + modifier) % list.length))
+      .map(({ src }) => ({ src } as SlideImage));
+
+    setLightboxItems(sortedList);
+    toggleLightbox();
+  }, []);
+
   return (
-    <div id='footage' className='flex justify-center px-4 py-8 sm:py-12 w-full relative bg-gold'>
-      <div className='w-full max-w-7xl h-full'>
-        <div>
-          <h3 className='header-signature text-5xl md:text-6xl lg:text-[72px] leading-none text-green'>
-            Luxury Footage
-          </h3>
-          <div className='w-full mt-4 block sm:hidden'>
-            <FootageMobile images={footageMobile} />
-          </div>
-          <div className='w-full mt-8 hidden sm:block'>
-            <div className='grid grid-cols-3 gap-4'>
-              {footageDesktop.slice(0, 3).map((item, index) => (
-                <div key={'item' + index + 1} className='w-full aspect-[3/2] relative block'>
-                  <Image
-                    src={item}
-                    alt='footage'
-                    className='object-cover' 
-                    fill
-                    priority
-                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                  />
-                </div>
-              ))}
+    <>
+      <div id='footage' className='flex justify-center px-4 py-8 sm:py-12 w-full relative bg-gold'>
+        <div className='w-full max-w-7xl h-full'>
+          <div>
+            <h3 className='header-signature text-5xl md:text-6xl lg:text-[72px] leading-none text-green'>
+              Luxury Footage
+            </h3>
+            <div className='w-full mt-4 block sm:hidden'>
+              <FootageMobile
+                images={footageMobile}
+                onClick={(index) => initializeLighboxItems(index, true)}
+              />
             </div>
-            <div className='h-12 my-4'></div>
-            <div className='grid grid-cols-3 gap-4'>
-              {footageDesktop.slice(3).map((item, index) => (
-                <div key={'item' + index + 4} className='w-full aspect-[3/2] relative block'>
-                  <Image
-                    src={item}
-                    alt='footage'
-                    className='object-cover' 
-                    fill
-                    priority
-                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                  />
-                </div>
-              ))}
+            <div className='w-full mt-8 hidden sm:block'>
+              <div className='grid grid-cols-3 gap-4'>
+                {footageDesktop.slice(0, 3).map((item, index) => (
+                  <div
+                    key={'item' + index + 1}
+                    className='w-full aspect-[3/2] relative block'
+                    onClick={() => initializeLighboxItems(index)}
+                  >
+                    <Image
+                      src={item}
+                      alt='footage'
+                      className='object-cover' 
+                      fill
+                      priority
+                      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className='h-12 my-4'></div>
+              <div className='grid grid-cols-3 gap-4'>
+                {footageDesktop.slice(3).map((item, index) => (
+                  <div
+                    key={'item' + index + 4}
+                    className='w-full aspect-[3/2] relative block'
+                    onClick={() => initializeLighboxItems(index + 3)}
+                  >
+                    <Image
+                      src={item}
+                      alt='footage'
+                      className='object-cover' 
+                      fill
+                      priority
+                      sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Lightbox
+        plugins={[Thumbnails, Fullscreen, Zoom]}
+        open={isOpenLightbox}
+        close={toggleLightbox}
+        slides={lightboxItems}
+      />
+    </>
   );
 };
 
